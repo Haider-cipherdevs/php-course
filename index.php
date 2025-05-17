ab2e05b723db196b57539c3577d9db125d505d75
Haider-cipherdevs <haider.cipherdevs@gmail.com>
2025-05-17 21:12:26 +0500

    testing

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
-<?php 
+<?php
 
 echo "Hello, World!";
+
+
+
+
+
+// $age = 17;
+// while ($age < 18) {
+//     echo "You are a minor.";
+//     $age++;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
